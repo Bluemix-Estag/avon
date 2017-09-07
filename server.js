e6b14cf8b497de4f06358af46b5a762c17d4b130
Rabah Zeineddine <rzeined@br.ibm.com>
2017-09-07 13:20:42 -0300

    Integrate with fake APIs

--- server.js
+++ server.js
@@ -2,19 +2,22 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var request = require('request');
-
-var PAGE_ACCESS_TOKEN = 'EAAHKrOfcyjkBAPENMxGEWySm1XwQoFXgGWRVUpQsrxPvbXXxm1eRZAIYKGPwEaaqnZCZCSnPswbmgQXReRUcenqvcKqVwZAylUrHPJRZA7AX7ZCNryeVKTYM2H6ZB0FBbFtdJv2TiND4f1g4ZBbDbPA6zuFV5hAOGPZCFvYI3uUZB8MgZDZD';
-var NODE_RED_URL = 'https://demos-node-red.mybluemix.net/avonConversation';
-
+var moment = require('moment');
 app.use(bodyParser.json());
-
 var port = process.env.PORT || 5000;
 app.set('port', port);
+
+var fs = require('fs')
+, ursa = require('ursa');
+key = ursa.createPrivateKey(fs.readFileSync('./key.pem'));
+
+require('dotenv').load(); // Load .env file to process.. 
+
 
 
 app.get('/facebook/receive', (req, res) => {
     if (req.query['hub.mode'] === 'subscribe' &&
-        req.query['hub.verify_token'] === 'avon_rzeined_token') {
+        req.query['hub.verify_token'] === process.env.FB_ACCESS_TOKEN) {
         console.log("Validating webhook");
         res.status(200).send(req.query['hub.challenge']);
     } else {
@@ -28,35 +31,49 @@ app.post('/facebook/receive', function (req, res) {
     var data = req.body;
     // Make sure this is a page subscription
     if (data.object === 'page') {
-
         // Iterate over each entry - there may be multiple if batched
         data.entry.forEach(function (entry) {
-   
             var pageID = entry.id;
             var timeOfEvent = entry.time;
             // Iterate over each messaging event
-
-            entry.messaging.forEach(function (event) {
-                if (event.message && pageID != event.sender.id) {
-                    receivedMessage(event);
-                } else if(event.referral) {
-
-                    decryptHash(event);
-
-                }else {
-                    console.log("Webhook received unknown event: ");
-                }
-            });
+            if (entry.messaging) {
+                entry.messaging.forEach(function (event) {
+                    if (event.message && pageID != event.sender.id) {
+                        receivedMessage(event);
+                    } else if (event.referral) {
+                        // Get Hash params using facebook messenger link..
+                        saveHashOnContext(event);
+                    } else {
+                        console.log("Webhook received unknown event: ");
+                    }
+                });
+            } else if (entry.standby) {
+                entry.standby.forEach(function (event) {
+                    if (event.postback) {
+                        receivedPostback(event);
+                    }
+                })
+            }
         });
-
-        // Assume all went well.
-        //
-        // You must send back a 200, within 20 seconds, to let us know
-        // you've successfully received the callback. Otherwise, the request
-        // will time out and we will keep trying to resend.
         res.sendStatus(200);
     }
 });
+
+
+function receivedPostback(event) {
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfPostback = event.timestamp;
+
+    // The 'payload' param is a developer-defined field which is set in a postback 
+    // button for Structured Messages. 
+    var title = event.postback.title;
+    console.log("Received postback for user %d and page %d with payload '%s' " +
+        "at %d", senderID, recipientID, title, timeOfPostback);
+    // When a postback is called, we'll send a message back to the sender to 
+    // let them know it was successful
+    sendTextMessage(senderID, title);
+}
 
 function receivedMessage(event) {
     var senderID = event.sender.id;
@@ -64,22 +81,14 @@ function receivedMessage(event) {
     var timeOfMessage = event.timestamp;
     var message = event.message;
 
-    // console.log("Received message for user %d and page %d at %d with message:",
-        // senderID, recipientID, timeOfMessage);
-    // console.log(JSON.stringify(message));
+    console.log("Received message for user %d and page %d at %d with message:",
+        senderID, recipientID, timeOfMessage);
 
     var messageId = message.mid;
-
     var messageText = message.text;
-    // var messageAttachments = message.attachments;
 
-    // if (messageText) {
     sendTextMessage(senderID, messageText);
-    // } else if (messageAttachments) {
-        // sendTextMessage(senderID, "Message with attachment received");
-    // }
 }
-
 
 function sendTextMessage(recipientId, messageText) {
     var messageData = {
@@ -90,30 +99,203 @@ function sendTextMessage(recipientId, messageText) {
             text: messageText
         }
     };
-
     var options = {
-        uri: NODE_RED_URL,
+        uri: process.env.NODE_RED_URL,
         method: 'POST',
         json: messageData
     }
 
-    function callback(error, response, body){
-        if(!error && response.statusCode == 200){
-            console.log('Retorno do node red ', body);
-            messageData.message.text = body.output.text[0]; // Watson Response.
-        }else{
-            messageData.message.text = 'Erro ocorreu';
+    function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            reactToContextAction(messageData, body);
+        } else {
+            messageData.message.text = 'Um erro ocorreu, tente mais tarde!';
+            callSendAPI(messageData);
         }
-        callSendAPI(messageData);
     }
-    request(options, callback);    
+    request(options, callback);
 }
+
+function reactToContextAction(messageData, watsonData) {
+    // var action = watsonData.context.action;
+    var action = getAction(watsonData); // Fake for test 
+    switch (action) {
+        case "representative/pending":
+            getRepresentativePending(messageData, watsonData);
+            break;
+        case 'calculatePendingPayment':
+            calculatePengindPayment(messageData, watsonData);
+            break;
+        default:
+            messageData.message.text = watsonData.output.text[0];
+            callSendAPI(messageData);
+            break;
+    }
+}
+
+// Mock actions..
+function getAction(watsonData) {
+    switch (watsonData.input.text) {
+        case 'debitos pendentes':
+            return 'representative/pending';
+            break;
+        case 'Sim':
+            return 'calculatePendingPayment'
+            break;
+        default:
+            return 'normal';
+            break;
+    }
+}
+
+function getRepresentativePending(messageData, watsonData) {
+
+    var hash = watsonData.context.hash;
+    var decrypted = decrypt(hash);
+    var options = {
+        uri: process.env.REPRESENTATIVE_INFORMATION_URL + '?mrktCd=BR&langCd=pt',
+        method: 'GET',
+        headers: {
+            devKey: process.env.devKey,
+            acctNr: decrypted.split("+")[0],
+            Authentication: 'Token',
+            'X-Sec-Token': decrypted.split("+")[1]
+        }
+    }
+
+    function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            body = JSON.parse(body);
+            saveRepresentativesOnContext(messageData, body.data.representatives[0]);
+            getPendingPayments(messageData, watsonData, body.data.representatives[0].profilePersonal.firstName);
+        } else {
+            messageData.message.text = "Um erro ocorreu ao validar seu debito pendente. tente mais tarde!"
+            callSendAPI(messageData);
+        }
+    }
+    request(options, callback);
+}
+
+
+function getPendingPayments(messageData, watsonData, userFirstName) {
+
+    console.log('Get pending payments method inoked..');
+    var hash = watsonData.context.hash;
+    var decrypted = decrypt(hash);
+
+    var options = {
+        uri: process.env.PENDING_PAYMENTS_URL + '?mrktCd=BR&langCd=pt',
+        method: 'GET',
+        headers: {
+            devKey: process.env.devKey,
+            acctNr: decrypted.split("+")[0],
+            Authentication: 'Token',
+            impersAccNr: 'UNKNOWN',
+            'X-Sec-Token': decrypted.split("+")[1]
+        }
+    }
+    function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            body = JSON.parse(body);
+
+            savePendingPaymentsOnContext(messageData, body);
+            buildButtonsMessage(messageData, body, userFirstName, function (messageData) {
+                callSendAPI(messageData);
+            });
+
+        } else {
+            messageData.message.text = "Um erro ocorreu ao validar seu debito pendente. tente mais tarde!"
+            callSendAPI(messageData);
+        }
+    }
+    request(options, callback);
+}
+
+function buildButtonsMessage(messageData, body, userFirstName, callback) {
+
+    messageData.message = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "button",
+                "text": userFirstName + ', você tem R$' + (body.amountPOff - body.amount) + ' em aberto, deseja que eu gere um novo boleto?',
+                "buttons": [
+                    {
+                        "type": "postback",
+                        "title": "Sim",
+                        "payload": "GENERATE_BOLETO"
+                    },
+                    {
+                        "type": "postback",
+                        "title": "Nao",
+                        "payload": "DONT_GENERATE"
+                    }
+                ]
+            }
+        }
+    }
+    callback(messageData);
+}
+
+
+function calculatePengindPayment(messageData, watsonData) {
+
+    var hash = watsonData.context.hash;
+    var decrypted = decrypt(hash);
+
+    var options = {
+        uri: process.env.CALCULATE_PENDING_URL + '?mrktCd=BR&langCd=pt',
+        method: 'POST',
+        headers: {
+            devKey: process.env.devKey,
+            acctNr: decrypted.split("+")[0],
+            Authentication: 'Token',
+            'X-Sec-Token': decrypted.split("+")[1],
+            'Content-Type': 'application/json'
+        }
+    }
+
+    buildPaymentCalculatingBody(watson.pendingPayments, options, function (options) {
+        request(options, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                body = JSON.parse(body);
+                messageData.message.text = 'calculando..';
+                callSendAPI(messageData);
+            } else {
+                messageData.message.text = "Um erro ocorreu ao validar seu debito pendente. tente mais tarde!"
+                callSendAPI(messageData);
+            }
+        });
+    })
+}
+
+function buildPaymentCalculatingBody(pendingPayments, options, callback) {
+
+    var body = {
+        ordNr: pendingPayments.orderNr,
+        cmpgnYr: pendingPayments.cmpgYr,
+        cmpgnNr: pendingPayments.cmpgnNr,
+        pymtSlpBrCdNr: pendingPayments.barCdNr,
+        slpNr: pendingPayments.billNr,
+        pymtDtFrCalc: moment.unix(pendingPayments.billingDt).format("YYYY-MM-DD"),
+        pymtDueDt: moment.unix(pendingPayments.dueDt).format("YYYY-MM-DD"),
+        ordOrgAmt: pendingPayments.amount,
+        billCnt: "1",
+        fineDscntAmt: "0",
+        intrstAmt: "90",
+        bnkcd: "033-7",
+        pymtSlpExpDt: moment.unix(pendingPayments.billXpirtnDt).format("YYYY-MM-DD")
+    }
+    options.body = body;
+    callback(options);
+}
+
 
 
 function callSendAPI(messageData) {
     request({
         uri: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: { access_token: PAGE_ACCESS_TOKEN },
+        qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
         method: 'POST',
         json: messageData
 
@@ -124,7 +306,7 @@ function callSendAPI(messageData) {
             console.log("Successfully sent generic message with id %s to recipient %s",
                 messageId, recipientId);
         } else {
-            console.error("Unable to send message. ", response.statusCode );
+            console.error("Unable to send message. ", response.statusCode);
             // console.error(response);
             console.error(error);
         }
@@ -132,13 +314,11 @@ function callSendAPI(messageData) {
 }
 
 
-function decryptHash(event){
-    
+function saveHashOnContext(event) {
+
     // Do the decypt function , copy it from old code.. 
     var hash = event.referral.ref;
-    // Call the function here.
-    var decrypted = 'username+token';
-    
+
     // Save it on context. 
     var recipientId = event.sender.id;
     var messageData = {
@@ -148,28 +328,64 @@ function decryptHash(event){
         message: {
             text: ' '
         },
-        hash: decrypted
+        hash: hash
     };
 
     var options = {
-        uri: NODE_RED_URL,
+        uri: process.env.NODE_RED_URL,
         method: 'POST',
         json: messageData
     }
 
-    function callback(error, response, body){
-        if(!error && response.statusCode == 200){
+    function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
             console.log('Hash Saved on context');
-        }else{
+        } else {
             console.log('Um erro ecorreu!');
         }
     }
-    request(options, callback); 
-
+    request(options, callback);
 }
 
+function saveRepresentativesOnContext(messageData, representatives) {
+    // Save it on context. 
+    messageData.representatives = representatives;
+    var options = {
+        uri: process.env.NODE_RED_URL,
+        method: 'POST',
+        json: messageData
+    }
+    function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log('representatives Saved on context');
+        } else {
+            console.log('Um erro ecorreu!');
+        }
+    }
+    request(options, callback);
+}
 
+function savePendingPaymentsOnContext(messageData, pendingPayments) {
+    messageData.pendingPayments = pendingPayments;
+    var options = {
+        uri: process.env.NODE_RED_URL,
+        method: 'POST',
+        json: messageData
+    }
+    function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log('pendingPayments Saved on context');
+        } else {
+            console.log('Um erro ecorreu!');
+        }
+    }
+    request(options, callback);
+}
 
+function decrypt(hash) {
+    // Find the correct way
+    return key.decrypt(hash, 'base64', 'utf8');
+}
 // Listen on the specified port
 app.listen(port, function () {
     console.log('Client server listening on port ' + port);
